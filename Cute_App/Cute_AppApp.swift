a7810155179aaeffd926a36e614e92b4be93407e
konstantinos koliolios <kolioliosk@gmail.com>
2024-11-14 03:41:53 +0000

    Initial Commit

---
+++ Cute_App/Cute_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cute_AppApp.swift
+//  Cute_App
+//
+//  Created by Kostas Koliolios on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cute_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cute_AppTests/Cute_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cute_AppTests.swift
+//  Cute_AppTests
+//
+//  Created by Kostas Koliolios on 14/11/2024.
+//
+
+import Testing
+@testable import Cute_App
+
+struct Cute_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
